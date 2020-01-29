b09da7e387da5f42abbb867143cc5af42a554125
Your Name <you@example.com>
2020-01-29 12:59:42 -0800

    add the feature of get project by title; make new student

--- hackbright.py
+++ hackbright.py
@@ -31,9 +31,14 @@ def get_student_by_github(github):
 
     db_cursor = db.session.execute(QUERY, {'github': github})
 
+    # print(db_cursor, type(db_cursor))
+
     row = db_cursor.fetchone()
 
-    print("Student: {} {}\nGitHub account: {}".format(row[0], row[1], row[2]))
+    # print(row)
+
+    if row:
+        print("Student: {} {}\nGitHub account: {}".format(row[0], row[1], row[2]))
 
 
 def make_new_student(first_name, last_name, github):
@@ -42,12 +47,32 @@ def make_new_student(first_name, last_name, github):
     Given a first name, last name, and GitHub account, add student to the
     database and print a confirmation message.
     """
-    pass
+    QUERY = """ 
+            INSERT INTO students (first_name, last_name, github)
+            VALUES (:first_name, :last_name, :github)
+            """
+
+    db.session.execute(QUERY, {'first_name': first_name,
+                               'last_name': last_name,
+                               'github': github})
+
+    db.session.commit()
+
+    print(f'Successfully added student: {first_name} {last_name}')
 
 
 def get_project_by_title(title):
     """Given a project title, print information about the project."""
-    pass
+    QUERY = """ 
+            SELECT * FROM projects
+            WHERE title = :title
+            """
+
+    cursor = db.session.execute(QUERY, {'title': title})
+
+    row = cursor.fetchone()
+
+    print(f'{row}')
 
 
 def get_grade_by_github_title(github, title):
@@ -91,7 +116,7 @@ def handle_input():
 if __name__ == "__main__":
     connect_to_db(app)
 
-    # handle_input()
+    #handle_input()
 
     # To be tidy, we close our database connection -- though,
     # since this is where our program ends, we'd quit anyway.
